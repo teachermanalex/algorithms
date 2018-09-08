d533959a1aba8583dc349f4c307c870c392e2c92
Alex Pakter <alexpakter@gmail.com>
2018-09-08 09:47:48 -0700

    fixed error, removed loop to shift left pointer

--- maximalSubArray.js
+++ maximalSubArray.js
@@ -1,35 +1,36 @@
 // For an array of numbers, return the maximal substring in O(n) time
 
+if (typeof(alert) == 'undefined') {
+	global.alert = mesg => console.log(mesg);
+}
+
 function getMaxSubSum(arr) {
-let maxsum=Number.NEGATIVE_INFINITY, bestLeft=0, bestRight=0;
+	let maxsum=Number.NEGATIVE_INFINITY, bestLeft=0, bestRight=0;
 
-// Find the maximal subarray sum from index 0 to n, where n<arr.length
-for (let i=0, currsum=0; i<arr.length; i++) {
-currsum+=arr[i];
-if (currsum > maxsum) {
-maxsum=currsum;
-bestRight=i+1;
-}
-}
+	// Find the maximal subarray sum from index 0 to n, where n<arr.length
+	let currLeft=0;
+	for (let i=0, currsum=0; i<arr.length; i++) {
+		if (currsum < 0) {
+			currsum=0;
+			currLeft=i;
+		}
 
-// Move the left pointer to the right, shortening the maximal subarray
-// to find the largest possible subarray
-for (let i=0, currsum=maxsum; i<bestRight; i++) {
-currsum -= arr[i];
-if (currsum > maxsum) {
-maxsum=currsum;
-bestLeft=i+1;
-}
-}
+		currsum+=arr[i];
+		if (currsum > maxsum) {
+			maxsum=currsum;
+			bestRight=i+1;
+			bestLeft=currLeft;
+		}
+	}
 
-  if (maxsum <= 0) {
-    maxsum=0;
-    bestLeft=0;
-    bestRight=0;
-  }
+	if (maxsum <= 0) {
+		maxsum=0;
+		bestLeft=0;
+		bestRight=0;
+	}
   
-alert(`For this array:${arr}\nMaxSum of ${maxsum} acheived with subarray indexes ${bestLeft} to ${bestRight}\nYielding this subarray: ${arr.slice(bestLeft, bestRight)}`);
-return maxsum;
+	alert(`For this array:${arr}\nMaxSum of ${maxsum} acheived with subarray indexes ${bestLeft} to ${bestRight}\nYielding this subarray: ${arr.slice(bestLeft, bestRight)}`);
+	return maxsum;
 }
 
 getMaxSubSum([-1, 2, 3, -9]);
@@ -39,3 +40,4 @@ getMaxSubSum([-2, -1, 1, 2]);
 getMaxSubSum([100, -9, 2, -3, 5]);
 getMaxSubSum([1, 2, 3]);
 getMaxSubSum([-1, -2]);
+getMaxSubSum([1, 2, -1000, 5]);
